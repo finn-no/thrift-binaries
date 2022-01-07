b33096404e30685035e3b2db9bfa29fadaecbf82
henrik242 <henrik242@users.noreply.github.com>
2022-01-07 14:28:42 +0100

    Use variables

--- download_thrift.sh
+++ download_thrift.sh
@@ -2,17 +2,20 @@
 
 set -o errexit -o pipefail -o nounset
 
+VERSION=0.9.1
+NAME=thrift-$VERSION
+
 if [[ $(uname -s) == "Darwin" ]]; then
-  FILE=thrift-0.9.1-darwin
+  FILE=$NAME-darwin
 else
-  FILE=thrift-0.9.1-linux
+  FILE=$NAME-linux
 fi
 
 echo "downloading thrift from github"
 mkdir -p bin
-curl -sfo bin/thrift-0.9.1 https://raw.githubusercontent.com/finn-no/thrift-binaries/main/$FILE
+curl -sfo bin/$NAME https://raw.githubusercontent.com/finn-no/thrift-binaries/main/$FILE
 
-chmod 755 bin/thrift-0.9.1
-ln -s thrift-0.9.1 bin/thrift
+chmod 755 bin/$NAME
+ln -s $NAME bin/thrift
 
 echo "$(bin/thrift --version) downloaded and installed to $(pwd)/bin"
